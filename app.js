6d8fb12d574d4d30fed360a292cb293882e4906b
Oleksii <kespa71for@gmail.com>
2025-10-30 11:35:02 +0100

    feat(app): create routing

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const Logger = (req, res, next) => {
+		console.log("port")
+		next()
+}
+app.use(Logger)
+
 app.get('/', (req, res) => {
 		res.send('Hello World!')
 })
-
+app.get("/about", (req, res) => {
+		res.send('About Page')
+})
+app.get("/about/:username", (req, res) => {
+		res.json({
+				message: `Hello World! + ${req.params.username}`,
+				query: req.query
+		})
+})
 app.listen(port, () => {
 		console.log(`Example app listening on port ${port}`)
 })
